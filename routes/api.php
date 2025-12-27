83277d3af91d3056a7a51b170e4c184301dfbd9d
thays kelsiane <thayskelsyane00@gmail.com>
2025-12-26 22:18:28 -0300

    integracao frotend com API de delegacias

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,19 @@
+
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
 
-// REGISTER
-Route::post('/register', [AuthController::class, 'register']);
-
-// LOGIN
-Route::post('/login', [AuthController::class, 'login']);
-
-// ROTAS PÚBLICAS
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-
-// ROTAS PROTEGIDAS
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::patch('/posts/{post}', [PostController::class, 'update']);
-    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+Route::get('/delegacias', function () {
+    return response()->json([
+        [
+            'id' => 1,
+            'nome' => 'Delegacia da Mulher – Centro',
+            'cidade' => 'Sua cidade'
+        ],
+        [
+            'id' => 2,
+            'nome' => 'Delegacia da Mulher – Zona Norte',
+            'cidade' => 'Sua cidade'
+        ]
+    ]);
 });
